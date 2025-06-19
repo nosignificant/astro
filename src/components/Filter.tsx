523428ee075eae40b39c54dc0fdfcd7da28d9835
nosignificant <nosignificantment@gmail.com>
2025-06-19 18:01:02 +0900

    0619

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,5 @@
 import { notePosts } from "../lib/utils/note-post";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 let filterTag = [];
 
@@ -7,6 +7,13 @@ export default function Filter() {
   const { posts, tagsSet } = notePosts();
   const [selectedTag, setSelectedTag] = useState<string[]>(tagsSet); // 초기에는 전체 보이게
   const [firstSelected, setFirstSelected] = useState(true);
+
+  useEffect(() => {
+    if (!firstSelected && selectedTag.length === 0) {
+      setSelectedTag(tagsSet);
+      setFirstSelected(true);
+    }
+  }, [firstSelected, selectedTag]);
 
   function clearTags() {
     setFirstSelected(true);
@@ -17,7 +24,7 @@ export default function Filter() {
     const filteredPosts = posts.filter((post) =>
       selectedTag.some((tag) => post.tags.includes(tag))
     );
-    console.log("selected tag", [...selectedTag]);
+    //console.log("selected tag", [...selectedTag]);
     return filteredPosts;
   }
 
@@ -38,32 +45,38 @@ export default function Filter() {
   return (
     <div className="flex flex-col divide-y filter">
       <div className="flex flex-wrap">
-        {tagsSet.map((tag) => (
-          <button
-            key={tag}
-            className="flex flex-row pr-4 pl-4 pd-2 border rounded-[30px] border-black hover:bg-gray-200"
-            onClick={() => tagClicked(tag)}
-          >
-            {tag}
-          </button>
-        ))}
+        {tagsSet.map((tag) => {
+          const isSelected = selectedTag.includes(tag) && !firstSelected;
+          return (
+            <button
+              key={tag}
+              className={`flex flex-row pr-4 pl-4 pd-2 rounded-[30px] border 
+        ${isSelected ? "bg-gray-200" : "border-black"}`}
+              onClick={() => tagClicked(tag)}
+            >
+              {tag}
+            </button>
+          );
+        })}
         <a
-          className=" pr-4 pl-4 pd-2 border rounded-[30px] border-black hover:bg-gray-200"
+          className=" text-[0.7rem] text-gray-700 pr-2 pl-2 "
           onClick={clearTags}
         >
-          X
+          필터 초기화
         </a>
       </div>
       <div className="mb-4" />
       {postFilter().map(({ url, title, date, tags }) => (
         <div key={url} className="entry-row">
-          <a href={url} className="flex-1 truncate">
+          <a href={url} className="flex-1 truncate ">
             {title}
-            {tags.map((tag) => (
-              <div key={tag} className="text-gray-500 text-sm">
-                {tag}
-              </div>
-            ))}
+            <div className="flex flex-row">
+              {tags.map((tag) => (
+                <div key={tag} className="text-gray-500 text-sm pr-2 ">
+                  {tag}
+                </div>
+              ))}
+            </div>
           </a>
           <div className="text-gray-500 hidden sm:block pr-4">{date}</div>
         </div>
